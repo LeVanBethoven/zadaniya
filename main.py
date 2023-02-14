8ec353c80ef60ce981a537e08d83cd0aae9b8fa1
LeVanBethoven <124084073+LeVanBethoven@users.noreply.github.com>
2023-02-14 11:17:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+tseloe = 10
+drobnoye = float (8.4)
+stroka = str ('No')
+print (tseloe)
+print (drobnoye)
+print (stroka)
+big_netseloe = tseloe*3.5
+print (big_netseloe)
+drobnoye = drobnoye - 1
+print (drobnoye)
+print (tseloe/drobnoye)
+print (big_netseloe/drobnoye)
+stroka = "No"*2+"_"+"Yes"*3
+print (stroka) 
