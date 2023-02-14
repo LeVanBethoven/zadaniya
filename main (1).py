7083946fcb080b680db357d3611b8bd90bbc003e
LeVanBethoven <124084073+LeVanBethoven@users.noreply.github.com>
2023-02-14 11:32:57 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+tseloe = 10
+drobnoye = 8
+tseloe < drobnoye and drobnoye < drobnoye
+tseloe and drobnoye > drobnoye + tseloe
+tseloe > drobnoye and drobnoye < drobnoye
+tseloe and drobnoye < drobnoye + tseloe
