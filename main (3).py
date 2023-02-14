22325c7f59a48e3612d37a94a0f147a5d45828ae
LeVanBethoven <124084073+LeVanBethoven@users.noreply.github.com>
2023-02-14 12:00:13 +0300

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,15 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+dark=input ("Вы хотите спать (Да, Нет, Не знаю)")
+if dark == "Да":
+    print ('Идите спать')
+elif dark=="Не знаю":
+    print('Подумайте ещё')
+else:
+    print ('Идите гулять')
