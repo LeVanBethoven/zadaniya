2cbf1334a7df0eeb1d937018cd3664fed9dad92c
LeVanBethoven <124084073+LeVanBethoven@users.noreply.github.com>
2023-02-28 11:39:50 +0300

    Add files via upload

---
+++ main (4).py
@@ -0,0 +1,18 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+print ('Введте число : ')
+a = 1
+b = 0
+c = 0
+a = (int(input()))
+b = a
+while a >> 0 :
+    b = a%10
+    a = a//10
+    print (b)
+
