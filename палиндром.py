6a750acabfd098033d21c6aefec734d46e7d4153
LeVanBethoven <124084073+LeVanBethoven@users.noreply.github.com>
2023-02-28 11:45:51 +0300

    Add files via upload

---
+++ палиндром.py
@@ -0,0 +1,16 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+print ('Введите число : ')
+a = 1
+a = (int(input()))
+if (a % 10 == a // 100) :
+    print ('палиндром')
+
+
+    
+
