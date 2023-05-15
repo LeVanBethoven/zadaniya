b9a9285139b487f24ec28bb346856948746062a5
LeVanBethoven <124084073+LeVanBethoven@users.noreply.github.com>
2023-05-15 13:05:35 +0300

    first 15 tasks.py

--- main (30).py
+++ main (30).py
@@ -1,3 +1,99 @@
 x=int(input())
 x=7*x+5
 print (x)
+
+
+x=int(input())
+x=x*x
+print (x)
+
+
+x=float(input())
+x=2*3.1417926*x
+print (x)
+
+
+  x=float(input())
+x=x*x*3.1415926
+print (x)
+
+
+import math
+x=int(input())
+y=int(input())
+x=math.sqrt (x*x+y*y)
+print (x)
+
+
+import math
+x=int(input())
+y=int(input())
+z=int()
+z=math.sqrt (x*x+y*y)
+print (x+y+z)
+
+
+x=int(input())
+z=(x*x*6)
+print (z)
+z=(x*x*x)
+print (z)
+
+
+import math
+x=int(input())
+y=int(input())
+z=int(input())
+a=int()
+b=int()
+a=x-y
+b=math.sqrt (a*a+z*z)
+z=z+a+b
+x=(2*b+x+y)
+print (x)
+
+
+x=float(input())
+y=float(input())
+x=x/y
+print (x)
+
+
+x=float(input())
+y=float(input())
+z=float()
+z=x+y
+print (z)
+z=x-y
+print (z)
+z=x*y
+print (z)
+z=x/y
+print (z)
+
+
+import math
+x=float(input())
+y=float(input())
+z=float()
+z=x/2+y/2
+print (z)
+z = math.sqrt (x*y)
+print (z)
+
+
+x=float(input())
+y=float(input())
+z=float()
+z=y/x
+print (z)
+
+
+x=float(input())
+y=float(input())
+z=float(input())
+a=float()
+a=x*y*2+x*z*2+y*z*2 
+print (a)
+a=x*y*z
+print (a)
